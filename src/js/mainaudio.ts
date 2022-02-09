4497210445551b41c8c4c57211c79664aa72b252
BSKOM <b.shelestua@gmail.com>
2022-02-09 19:12:47 +0200

    fix:fix mainaudio

--- src/js/mainaudio.ts
+++ src/js/mainaudio.ts
@@ -2,9 +2,17 @@
 /* eslint no-console:  */
 import PlayAudio from './playaudio';
 import data from './data';
+import { audio } from './audio';
+// import '../assets/rs-school-footer.svg';
 
-const i = 1;
-const di = data[i];
-const srca = `assets/${di.audio}`;
-PlayAudio(srca);
-console.log(`word: ${di.word} `);
+function startAudio(): void {
+  const i = 1;
+  const di = data[i];
+  const srca = `assets/${di.audio}`;
+  PlayAudio(srca);
+  audio.src = 'assets/files/01_0002.mp3';
+  const imgt = document.querySelector('.image');
+  (<HTMLImageElement>imgt).src = `assets/${di.image}`;
+  console.log(`word: ${di.word} `);
+}
+export default startAudio;
