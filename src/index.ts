fa7514d495ac3d8ff86eb0db58e86003f537462d
Vladislav <noreply@github.com>
2022-02-09 11:02:01 +0300

    docs: update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 /* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable import/extensions */
+/* eslint-disable linebreak-style */
 
 import './style.scss';
 import Sprint from './sprint/script.js';
@@ -7,4 +10,5 @@ const sprint = new Sprint();
 sprint.start();
 
 // eslint-disable-next-line import/extensions
+// eslint-disable-next-line import/first
 import './page-input/index.ts';
