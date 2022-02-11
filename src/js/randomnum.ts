85aff8ba5593ccc4c76119cc17a11acc06772dc4
BSKOM <b.shelestua@gmail.com>
2022-02-11 21:02:35 +0200

    refactor: randomnum rename function

--- src/js/randomnum.ts
+++ src/js/randomnum.ts
@@ -1,1 +1,1 @@
-export const RandomNum = (min: number, max : number):number => (Math.round(Math.random() * (max - min) + min));
+export const randomNum = (min: number, max : number):number => (Math.round(Math.random() * (max - min) + min));
