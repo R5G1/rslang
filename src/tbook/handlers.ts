707e6e676322c0e086c46760b7040a4f8ca7d343
BSKOM <b.shelestua@gmail.com>
2022-02-21 06:17:37 +0200

    fix:fix handlers

--- src/tbook/handlers.ts
+++ src/tbook/handlers.ts
@@ -1,17 +1,17 @@
-
-import API from '../js/api';
+import API from './api';
 import sortWords from './sortwords';
 // import sortWords from '../js/sortwords';
-import { updRender } from "./rendbooks";
+import { updRender } from './rendbooks';
 
-export const pag = (trg:HTMLElement) => {
-  const pageNum = (Number(trg.textContent)-1)%30;
-  const groupNum = Math.floor((Number(trg.textContent)-1 )/30) ;
-  console.log('I Am Pag!', pageNum, groupNum )
-  const api = new API;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const pag = (trg: HTMLElement) => {
+  const pageNum = (Number(trg.textContent) - 1) % 30;
+  const groupNum = Math.floor((Number(trg.textContent) - 1) / 30);
+  // console.log('I Am Pag!', pageNum, groupNum )
+  const api = new API();
   const prom = api.getWords(groupNum, pageNum);
   prom.then((value) => {
     const words = value;
     updRender(sortWords(words));
-  })
-}
+  });
+};
